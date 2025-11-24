0e12a1fffdba1fb4400a089379ea43bdcbd8e181
Vladyslav <vladislavlyevshakov@gmail.com>
2025-11-24 22:11:34 +0200

    fixed editArticle iissues

--- tests/_fixtures/fixturesArticles.ts
+++ tests/_fixtures/fixturesArticles.ts
@@ -2,13 +2,11 @@ import { test as base } from '@playwright/test';
 import { Logger } from '../../src/common/logger/Logger';
 import { CreateArticlePage } from '../../src/ui/pages/article/CreateArticlePage';
 import { ViewArticlePage } from '../../src/ui/pages/article/ViewArticlePage';
-import { EditArticlePage } from '../../src/ui/pages/article/EditArticlePage';
 import { generateNewArticleData } from '../../src/common/testData/generateNewArticleData';
 
 export const test = base.extend<{ 
   createArticlePage: CreateArticlePage;
   viewArticlePage: ViewArticlePage;
-  editArticlePage: EditArticlePage;
   articleWithoutTags;
   articleWithOneTag;
   articleWithTwoTags;
@@ -25,11 +23,6 @@ export const test = base.extend<{
   viewArticlePage: async ({ page }, use) => {
     const viewArticlePage = new ViewArticlePage(page);
     await use(viewArticlePage);
-  },
-
-  editArticlePage: async ({ page }, use) => {
-    const editArticlePage = new EditArticlePage(page);
-    await use(editArticlePage);
   },
 
   articleWithoutTags: async ({ logger }, use) => {
